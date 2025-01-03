39e9067ef4dd905d4fe1fa812ae224f82f6a5ac0
KJ1002 <nguyenvanphidmx123455@gmail.com>
2025-01-04 00:40:18 +0700

    git commit -m commit git commit -m

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,7 +3,9 @@ import { LoginComponent } from '@/components'
 const Login = () => {
     return (
         <div className='dark:bg-blackPrimary bg-whiteSecondary min-h-[100vh] w-full flex justify-center items-center py-10'>
-            <LoginComponent />
+            {/* <LoginComponent /> */}
+
+            <img src='https://res.cloudinary.com/demo/image/upload/w_500,h_500,c_fill/iphone15.jpg' alt='erorr image' />
         </div>
     )
 }
